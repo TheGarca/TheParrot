12058b869eb51fbfff06ffb10f643c0d99b07aef
Eduardo Queiroz <e-q-ps@hotmail.com>
2018-04-18 18:49:08 -0300

    teste
    Versao que funciona

--- src/com/theparrot/controller/TheParrot.java
+++ src/com/theparrot/controller/TheParrot.java
@@ -19,7 +19,7 @@ public class TheParrot extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../view/Login.fxml"));
         
         Scene scene = new Scene(root);
         
